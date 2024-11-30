57f7ad1d85c94ba24992fce0e90743d292b1659f
intJack <14380868+intjack@user.noreply.gitee.com>
2024-11-30 20:15:03 +0800

    去除了mysql-connector-java依赖

--- resource_management_system/src/test/java/edu/just/resource_management_system/springbootApplicationTests.java
+++ resource_management_system/src/test/java/edu/just/resource_management_system/springbootApplicationTests.java
@@ -3,6 +3,7 @@ package edu.just.resource_management_system;
 import edu.just.resource_management_system.mapper.CarouselMapper;
 import edu.just.resource_management_system.mapper.LanguageMapper;
 import edu.just.resource_management_system.mapper.TagMapper;
+import edu.just.resource_management_system.mapper.UserMapper;
 import edu.just.resource_management_system.pojo.Admin;
 import edu.just.resource_management_system.pojo.User;
 import edu.just.resource_management_system.service.AdminService;
@@ -20,6 +21,37 @@ class springbootApplicationTests {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserMapper userMapper;
+    /**
+     * 这是user登录接口的测试
+     */
+    @Test
+    void UserMapperLoginTest(){
+        /**
+         * 从前端接收到参数username password
+         * 返回user 登录成功user不为空 登录失败为空
+         */
+        User user = userMapper.login("...", "...");
+        if (user == null){
+            //重新输入
+        }
+        else {
+            //登录成功的业务
+        }
+    }
+
+    /**
+     *这是用户注册的接口
+     * 不用担心 springmvc 会自动将表单数据转换成user
+     * 你要做的就是在controller形参添加 User user
+     */
+    @Test
+    void UserMapperSetPwdTest(){
+        userMapper.register(new User());
+    }
+
     @Test
     void testUserMapper(){
         User user = userService.findUserById(1L);
