1bdd4200c83be13e700d6cd0edacd2d76e098c1f
intJack <14380868+intjack@user.noreply.gitee.com>
2024-12-02 10:01:44 +0800

    实现了登录，注册的业务功能。当登录成功后，home页面会显示个人信息。暂时注释掉了CustomUserDatailService和SecurityConfig。删除了login.html。

--- resource_management_system/src/main/java/edu/just/resource_management_system/springbootApplication.java
+++ resource_management_system/src/main/java/edu/just/resource_management_system/springbootApplication.java
@@ -3,7 +3,6 @@ package edu.just.resource_management_system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @MapperScan(basePackages = "edu.just.resource_management_system.mapper")
 @SpringBootApplication
