3bcadb10fa2a0795923acefd658dbe5bf15bba28
intJack <14380868+intjack@user.noreply.gitee.com>
2024-12-19 08:52:22 +0800

    添加了切面类，并实现了记录管理员登录日志

--- resource_management_system/src/main/java/edu/just/resource_management_system/config/WebConfig.java
+++ resource_management_system/src/main/java/edu/just/resource_management_system/config/WebConfig.java
@@ -1,6 +1,7 @@
 package edu.just.resource_management_system.config;
 
 import edu.just.resource_management_system.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 注册 拦截器 实现对于token失效或者缺少token 的拦截功能
  * 只有在用户点击下载资源的时候 才会触发拦截功能
  */
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
